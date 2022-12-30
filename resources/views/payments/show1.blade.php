6b637765fe07b9f3527cd50a00f24f852da5291c
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-12-30 19:10:54 +0500

    Vue setup

--- resources/views/payments/show1.blade.php
+++ resources/views/payments/show1.blade.php
@@ -2,151 +2,180 @@
 @section('content')
 @section('title')
 <nav class="navbar navbar-main navbar-expand-lg px-0 shadow-none border-radius-xl">
-   <div class="container-fluid ">
-   <nav aria-label="breadcrumb">
-        <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+  <div class="container-fluid ">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
         <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Admin</a></li>
         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Payments</li>
         <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Single Payment</li>
-        </ol>
+      </ol>
     </nav>
-   </div>
+  </div>
 </nav>
 @endsection
-<div id="modal-show" class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-sm">
-    <div class="modal-content">
-      ...
-    </div>
-  </div>
-</div>
-<div class="row mt-2 p-2">
+<div id="app">
+  <div class="row mt-2 p-2">
     <div class="col m-2">
-        <div class="card mt-2">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h5 class="font-weight-bolder mb-0">Payment Details</h5>
-                    </div>
-                    
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-md-6">
-                        <b>Customer No.</b>
-                        <p>{{$payment->user->customer_no}}</p>
-                    </div>
-                    <div class="col-md-6">
-                        <b>Company Name</b>
-                        <p>{{$payment->user->name}}</p>
-                    </div>
-                    
-                    <div class="col-md-6">
-                        <b>Amount</b>
-                        <p>{{convert_currency($payment->amount)}}</p>
-                    </div>
-                    <div class="col-md-6">
-                        <b>Status</b>
-                        <p><span class="badge badge-sm {{$payment->status == 0 ? 'badge-secondary' : 'badge-success'}}">{{$payment->status == 0 ? 'PENDING ACKNOWLEDGEMENT' : 'ACKNOWLEDGED'}}</span></p>
-                    </div>
-                
-                    <div class="col-md-6">
-                        <b>Proof Document</b>
-                        <p><a style="color:#009fe3;" href="{{route('payments.download',$payment->id)}}">
-                            {{$payment->proof}}</a></p>
-                    </div>
-                    <div class="col-md-6">
-                        <b>Payment Date</b>
-                        <p>{{date_formatter($payment->payment_date)}}</p>
-                    </div>
-                </div>
+      <div class="card mt-2">
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="font-weight-bolder mb-0">Payment Details</h5>
             </div>
+
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-md-6">
+              <b>Customer No.</b>
+              <p v-text="payment.user?.customer_no"></p>
+            </div>
+            <div class="col-md-6">
+              <b>Company Name</b>
+              <p v-text="payment.user?.name"></p>
+            </div>
+
+            <div class="col-md-6">
+              <b>Amount</b>
+              <p v-text="payment.amount"></p>
+            </div>
+            <div class="col-md-6">
+              <b>Status</b>
+              <p><span class="badge badge-sm" :class="{
+                'badge-secondary': payment.status == 0,
+                'badge-success': payment.status == 1
+              }" v-text="payment.status == 0 ? 'PENDING ACKNOWLEDGEMENT' : 'ACKNOWLEDGED'"></span></p>
+            </div>
+
+            <div class="col-md-6">
+              <b>Proof Document</b>
+              <p><a style="color:#009fe3;" href="{{route('payments.download',$payment->id)}}" v-text="payment.proof"></a></p>
+            </div>
+            <div class="col-md-6">
+              <b>Payment Date</b>
+              <p v-text="payment.payment_date"></p>
+            </div>
+          </div>
         </div>
-    </div>
-</div>
-<div class="row mt-2 p-2">
-    <div class="col m-2">
-        <div class="card mt-2">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h5 class="font-weight-bolder mb-0">Invoices Involved</h5>
-                    </div>
-                    <div class="table-responsive">
-    <div class="dataTable-wrapper dataTable-loading no-footer sortable fixed-height fixed-columns">
-      <div class="dataTable-top">
-        
-        <table class="table align-items-center mb-0">
-            
-          <thead class="mt-2">
-            <tr>
-              <!--<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer Name</th>-->
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Actions</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Due Date</th>
-              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Invoice No.</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Do No.</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Amount (MYR)</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Outstanding (MYR)</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Invoice Doc</th>
-              <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Do Doc</th>
-              <!--<th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Created at</th>-->
-            </tr>
-          </thead>
-          <tbody>
-            @foreach($invoices as $invoice)
-            <tr data-id="{{$invoice->id}}">
-                <td>
-                    <p class="text-xs text-secondary mb-0 text-center">
-                            <i class="fa fa-usd text-success" title="Add Payment" style="cursor: pointer;" aria-hidden="true"></i>
-                    </p>
-                </td>
-                <td>
-                    <p class="text-xs text-secondary mb-0 text-center">{{date_formatter($invoice->date)}}</p>
-              </td>
-              <!--<td>-->
-              <!--  <div class="d-flex px-2 py-1">-->
-              <!--    <div class="d-flex flex-column justify-content-center">-->
-              <!--      <p class="text-xs text-secondary mb-0">{{$invoice->user->name}}</p>-->
-              <!--    </div>-->
-              <!--  </div>-->
-              <!--</td>-->
-              <td>
-                    <p class="text-xs text-secondary mb-0 text-center">{{$invoice->invoiceId}}</p>
-              </td>
-               <td>
-                    <p class="text-xs text-secondary mb-0 text-center">{{$invoice->do_no}}</p>
-              </td>
-              <td>
-                    <p class="text-xs text-secondary mb-0 text-center">{{number_format($invoice->amount,2)}}</p>
-              </td>
-              <td>
-              <p class="text-xs text-secondary mb-0 text-center">{{number_format($invoice->outstanding,2)}}</p>
-              </td>
-              <td>
-                    <p class="text-xs text-secondary mb-0 text-center"><a class="text-info" href="{{route('download_inv',$invoice->id)}}">{{$invoice->invoice_doc}}</a></p>
-              </td>
-             
-              <!--<td class="align-middle text-center">-->
-              <!--  <span class="text-secondary text-xs font-weight-bold">{{$invoice->created_at ?  $invoice->created_at->diffForHumans() : 'N/A'}}</span>-->
-              <!--</td>-->
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
       </div>
     </div>
   </div>
-                </div>
+  <div class="row mt-2 p-2">
+    <div class="col m-2">
+      <div class="card mt-2">
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="font-weight-bolder mb-0">Invoices Involved</h5>
             </div>
+            <div class="table-responsive">
+              <div class="dataTable-wrapper dataTable-loading no-footer sortable fixed-height fixed-columns">
+                <div class="dataTable-top">
+
+                  <table class="table align-items-center mb-0">
+
+                    <thead class="mt-2">
+                      <tr>
+                        <!--<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer Name</th>-->
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Actions</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Due Date</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center">Invoice No.</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Do No.</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Amount (MYR)</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Outstanding (MYR)</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Invoice Doc</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Do Doc</th>
+                        <!--<th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Created at</th>-->
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="invoice in invoices">
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center">
+                            <i class="fa fa-usd text-success" title="Add Payment" style="cursor: pointer;" aria-hidden="true" @click="selectInvoice(invoice)"></i>
+                          </p>
+                        </td>
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center" v-text="invoice.date"></p>
+                        </td>
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center" v-text="invoice.invoiceId"></p>
+                        </td>
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center" v-text="invoice.do_no"></p>
+                        </td>
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center" v-text="invoice.amount"></p>
+                        </td>
+                        <td>
+                          <p class="text-xs text-secondary mb-0 text-center" v-text="invoice.outstanding"></p>
+                        </td>
+                        <td>
+                        </td>
+
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  <div v-if="showModal" class="d-flex align-items-center justify-content-center position-fixed" style="inset: 0; background-color: rgba(0, 0, 0, .4);">
+    <div class="bg-white w-75 p-3" style="max-width: 500px">
+      <header>This is header</header>
+      <div>
+        <form @submit.prevent="makePayment">
+          <div class="mb-3">
+            <label for="payment-amount" class="form-label">Payment Amount</label>
+            <input type="number" step="0.01" class="form-control" id="payment-amount" placeholder="10000" v-model="paymentAmount" v-on:focus="selectInput">
+          </div>
+          <button type="submit" class="btn btn-secondary btn-sm">Pay</button>
+        </form>
+      </div>
+      <footerc class="float-end">
+        <button class="btn btn-primary btn-sm" @click="showModal = !showModal">Close</button>
+        </footer>
+    </div>
+  </div>
 </div>
 @endsection
 
 @section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue.global.prod.min.js" integrity="sha512-3CesFAr6COyDB22AiVG2erk2moD1FeL3VMx6UezptTW3qmYdcQhfv6yDGmH4ICNTxd0Rs2AbMQ0Q5lG7J/8n3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
-  $('table tbody tr').on('click', function() {
-        var id = $(this).data('id');
-  });
+  const data = <?php echo json_encode(['payment' => $payment, 'invoices' => $invoices]); ?>
+
+  const app = {
+    data() {
+      return {
+        payment: data.payment,
+        invoices: data.invoices,
+        showModal: true,
+        paymentAmount: 0,
+        selectedInvoice: null,
+      }
+    },
+    methods: {
+      makePayment() {
+        console.log(this.paymentAmount);
+      },
+      selectInput(e) {
+        e.target.select();
+      },
+      selectInvoice(invoice) {
+        this.selectedInvoice = {
+          ...invoice
+        };
+        this.showModal = true;
+        console.log(this.selectedInvoice);
+      }
+    }
+  };
+
+  Vue.createApp(app).mount('#app');
 </script>
 @endsection
